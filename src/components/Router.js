e54e26eefada597279ff9a662f17b138aab88cc3
jungsikyeo <mdotcom12@naver.com>
2020-11-15 23:51:55 +0900

    route.js modify

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React from "react";
-=======
-import React, { useState } from "react";
->>>>>>> a8941ebaf35da6dec5083bb81ea524226f5e2ec5
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
